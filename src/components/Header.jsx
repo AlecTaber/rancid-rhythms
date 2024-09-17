5674659e0709be76c4f879ce05a315704e70cd11
Alec Taber <alectaber12@gmail.com>
2024-09-16 19:41:15 -0700

    add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaMusic } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="bg-green-500 flex items-center justify-between p-4">
+        <FaMusic className="text-3xl text-white" />
+        <h1 className="text-5xl text-center">Rancid Rhythms</h1>
+        <button className="float-right bg-white mt-3 rounded-md pr-2 pl-2">Sign In</button>
+    </header>
+  );
+};
+
+export default Header;
