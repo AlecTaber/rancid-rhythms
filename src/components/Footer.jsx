0cafe9e47dbfa34f1b60c472fffa58bcabd4df2a
Alec Taber <alectaber12@gmail.com>
2024-09-16 19:40:42 -0700

    add Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-green-500">
+      <p className="text-center">© 2024 Alec Taber, Ashlin Lee, Jacob Menlove</p>
+    </footer>
+  );
+};
+
+export default Footer;
