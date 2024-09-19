1544e742f48cc20dc8c13095e83100355020b001
Ashlin Lee <ashlinlee98@gmail.com>
2024-09-18 18:19:29 -0600

    Implemented authMiddleware.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
+import bcrypt from 'bcrypt';
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/connection';
+import sequelize from '../config/connection.js';
 
-//User table
-
-const User = sequelize.define('user', {
+const User = (sequelize) => {
+  const UserModel = sequelize.define('User', {
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -11,14 +11,22 @@ const User = sequelize.define('user', {
         autoIncrement: true,
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [8],
-        },
-    },
-});
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  // Hash password before saving user
+  UserModel.beforeCreate(async (user) => {
+    user.password = await bcrypt.hash(user.password, 10);
+  });
+
+  return UserModel;
+};
+
+export default User;
